aa607de3986bfa73c41b662a70f2f99bc67ed657
wusd <nanxihope@163.com>
2025-07-18 10:17:48 +0800

    --

--- src/main/java/com/wusd/skeleton/SkeletonApplication.java
+++ src/main/java/com/wusd/skeleton/SkeletonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan
+@MapperScan("com.wusd.skeleton.mapper")
 public class SkeletonApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkeletonApplication.class, args);
