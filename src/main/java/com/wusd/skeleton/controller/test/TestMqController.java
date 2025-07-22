b42e66e402f486d311a00759afc17bde71789ef9
wusd <nanxihope@163.com>
2025-07-22 13:13:54 +0800

    关闭rocketmq,过于消耗资源

--- src/main/java/com/wusd/skeleton/controller/test/TestMqController.java
+++ src/main/java/com/wusd/skeleton/controller/test/TestMqController.java
@@ -1,28 +1,28 @@
-package com.wusd.skeleton.controller.test;
-
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author Wusd
- * @date 2025/7/17
- * @description
- */
-@RestController
-@RequestMapping("/test/mq")
-public class TestMqController {
-    @Autowired
-    private RocketMQTemplate rocketMQTemplate;
-    private String topic = "string";
-
-    @GetMapping("/send")
-    public void send() {
-        Message<String> message = MessageBuilder.withPayload("hello, RocketMQ").build();
-        rocketMQTemplate.send(topic, message);
-    }
-}
+//package com.wusd.skeleton.controller.test;
+//
+//import org.apache.rocketmq.spring.core.RocketMQTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.Message;
+//import org.springframework.messaging.support.MessageBuilder;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author Wusd
+// * @date 2025/7/17
+// * @description
+// */
+//@RestController
+//@RequestMapping("/test/mq")
+//public class TestMqController {
+//    @Autowired
+//    private RocketMQTemplate rocketMQTemplate;
+//    private String topic = "string";
+//
+//    @GetMapping("/send")
+//    public void send() {
+//        Message<String> message = MessageBuilder.withPayload("hello, RocketMQ").build();
+//        rocketMQTemplate.send(topic, message);
+//    }
+//}
--- src/main/java/com/wusd/skeleton/mq/MyListener.java
+++ src/main/java/com/wusd/skeleton/mq/MyListener.java
@@ -1,21 +1,21 @@
-package com.wusd.skeleton.mq;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.springframework.stereotype.Component;
-
-/**
- * @author Wusd
- * @date 2025/7/17
- * @description
- */
-@Component
-@RocketMQMessageListener(topic = "string", consumerGroup = "testGroup250717")
-@Slf4j
-public class MyListener implements RocketMQListener<String> {
-    @Override
-    public void onMessage(String s) {
-        log.info("MyListener.onMessage...topic->string,s->{}", s);
-    }
-}
+//package com.wusd.skeleton.mq;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+//import org.apache.rocketmq.spring.core.RocketMQListener;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author Wusd
+// * @date 2025/7/17
+// * @description
+// */
+//@Component
+//@RocketMQMessageListener(topic = "string", consumerGroup = "testGroup250717")
+//@Slf4j
+//public class MyListener implements RocketMQListener<String> {
+//    @Override
+//    public void onMessage(String s) {
+//        log.info("MyListener.onMessage...topic->string,s->{}", s);
+//    }
+//}
