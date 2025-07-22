75c1f928c234bc853eabe3308c810ba43f7454a2
wusd <nanxihope@163.com>
2025-07-22 15:28:39 +0800

    增加数据库

--- src/main/java/com/wusd/skeleton/controller/test/UserLikeController.java
+++ src/main/java/com/wusd/skeleton/controller/test/UserLikeController.java
@@ -1,33 +1,33 @@
-package com.wusd.skeleton.controller.test;
-
-import com.wusd.skeleton.constant.RocketConstant;
-import com.wusd.skeleton.entity.po.UserLike;
-import com.wusd.skeleton.service.UserLikeService;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author Wusd
- * @date 2025/7/21
- * @description
- */
-@RestController
-@RequestMapping("/userLike")
-public class UserLikeController {
-    @Autowired
-    private RocketMQTemplate rocketMQTemplate;
-
-    @GetMapping("/{userId}")
-    public Boolean like(@PathVariable("userId") Long userId) {
-        // syncSend() 同步发送
-        // asyncSend() 异步发送
-        // sendOneWay() 单向发送, 也是异步发送,不等待Broker返回确认,性能更高
-        rocketMQTemplate.sendOneWay(RocketConstant.Topic.USER_LIKE, MessageBuilder.withPayload(userId).build());
-        return true;
-    }
-}
+//package com.wusd.skeleton.controller.test;
+//
+//import com.wusd.skeleton.constant.RocketConstant;
+//import com.wusd.skeleton.entity.po.UserLike;
+//import com.wusd.skeleton.service.UserLikeService;
+//import org.apache.rocketmq.spring.core.RocketMQTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.support.MessageBuilder;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author Wusd
+// * @date 2025/7/21
+// * @description
+// */
+//@RestController
+//@RequestMapping("/userLike")
+//public class UserLikeController {
+//    @Autowired
+//    private RocketMQTemplate rocketMQTemplate;
+//
+//    @GetMapping("/{userId}")
+//    public Boolean like(@PathVariable("userId") Long userId) {
+//        // syncSend() 同步发送
+//        // asyncSend() 异步发送
+//        // sendOneWay() 单向发送, 也是异步发送,不等待Broker返回确认,性能更高
+//        rocketMQTemplate.sendOneWay(RocketConstant.Topic.USER_LIKE, MessageBuilder.withPayload(userId).build());
+//        return true;
+//    }
+//}
--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 9000x-wsl
- Source Server Type    : MySQL
- Source Server Version : 80042
- Source Host           : 172.23.141.84:3306
- Source Schema         : order_db
-
- Target Server Type    : MySQL
- Target Server Version : 80042
- File Encoding         : 65001
-
- Date: 20/07/2025 20:59:36
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -46,7 +30,7 @@ CREATE TABLE `t_order_2`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `t_student`;
 CREATE TABLE `t_student`  (
-  `student_id` bigint NOT NULL AUTO_INCREMENT,
+  `student_id` bigint NOT NULL,
   `student_name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `del_flag` tinyint NULL DEFAULT NULL,
   PRIMARY KEY (`student_id`) USING BTREE
